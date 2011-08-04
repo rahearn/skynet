d8124d7ae7a175afdc6763f28061822eb1586c4c
Ryan Ahearn <ryan@coshx.com>
2011-08-04 14:49:25 -0400

    Creates a static site builder.
    Copies files from source to
    destination with no processing

--- builder/static.rb
+++ builder/static.rb
@@ -4,7 +4,16 @@ module Builder
   class Static < Builder::Base
 
     def run
-      puts "Running..."
+      puts 'Static running...'
+      if repo_exists?
+        update_repo
+      else
+        create_repo
+      end
+
+      source_directory = File.join(@local_repo_path, @config[:builder_options][:source], '.')
+      FileUtils.cp_r(source_directory, @config[:builder_options][:destination])
+      puts 'Static finished'
     end
 
   end
