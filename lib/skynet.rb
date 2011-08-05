88de13e358342d77a1d7c0f1b2cd73485c772ac9
Ryan Ahearn <ryan@coshx.com>
2011-08-05 11:07:18 -0400

    Modifies Skynet class to work within the Skynet module

--- lib/skynet.rb
+++ lib/skynet.rb
@@ -1,34 +1,30 @@
 require 'sinatra/base'
 require 'yaml'
-require 'builder'
+require 'skynet/builder'
 
-class SkyNet < Sinatra::Base
+module Skynet
 
-  configure do
-    set :app_file,   'sky_net.rb'
-    set :config,     YAML.load_file(File.join(root, 'config.yml'))[environment]
-    set :repository, config[:repository]
-    set :builder,    config[:builder]
-  end
+  class Skynet < Sinatra::Base
 
-  configure :production do
-    enable :logging
-  end
-
-  get '/' do
-    send_file File.join(settings.public, 'index.html')
-  end
-
-  post '/skynet' do
-    payload = JSON.parse params[:payload]
-    if settings.repository == payload['repository']['url']
-      Builder.build :builder => settings.builder, :config => settings.config
-      "Thanks!"
-    else
-      puts "Wrong repository"
-      "Sorry, not configured for this repository"
+    configure do
+      set :app_file,   'skynet.rb'
+      set :config,     YAML.load_file(File.join(root, 'config.yml'))[environment]
+      set :repository, config[:repository]
+      set :builder,    config[:builder]
     end
+
+    post '/skynet' do
+      payload = JSON.parse params[:payload]
+      if settings.repository == payload['repository']['url']
+        Builder.build :builder => settings.builder, :config => settings.config
+        "Thanks!"
+      else
+        puts "Wrong repository"
+        "Sorry, not configured for this repository"
+      end
+    end
+
+    Builder.build(:builder => settings.builder, :config => settings.config) if settings.config[:build_on_startup]
   end
 
-  Builder.build(:builder => settings.builder, :config => settings.config) if settings.config[:build_on_startup]
 end
